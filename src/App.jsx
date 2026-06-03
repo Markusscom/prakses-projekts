b619e2a53b6de2cbe1378f491aef7e99dac8fb71
markusscom <markusscom69@gmail.com>
2026-06-03 14:14:39 +0300

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Start from "./pages/Start";
 import Dashboard from "./pages/Dashboard";
 import Join from "./pages/Join";
+import Live from "./pages/Live";
 import CreateQuiz from "./pages/CreateQuiz";
 import PlayQuiz from "./pages/PlayQuiz";
 import Results from "./pages/Results";
@@ -15,6 +16,7 @@ export default function App() {
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/create" element={<CreateQuiz />} />
                 <Route path="/join" element={<Join />} />
+                <Route path="/live" element={<Live />} />
                 <Route path="/play/:id" element={<PlayQuiz />} />
                 <Route path="/results" element={<Results />} />
 
