b9ea15c964eba5f2a735cb70d0de15609bdf6452
markusscom <markusscom69@gmail.com>
2026-06-03 13:33:07 +0300

    dashboard lapa skolotājiem

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Dashboard() {
+    const navigate = useNavigate();
+
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h1>Teacher Dashboard</h1>
+
+            <button onClick={() => navigate("/create")}>
+                Create Quiz
+            </button>
+
+            <button onClick={() => navigate("/live")}>
+                Start Live Game
+            </button>
+        </div>
+    );
+}
